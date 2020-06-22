1fa9b0e8d5b5b9303a5c05cbb2236bcd24fa73fd
Alex Aquino <alexqno@gmail.com>
2020-06-22 14:02:51 -0300

    Cria usuario kamaleon na migration

--- src/database/migrations/20200610130853-create-users.js
+++ src/database/migrations/20200610130853-create-users.js
@@ -1,34 +1,46 @@
+import User from '../../app/models/User';
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    });
+    return queryInterface
+      .createTable('users', {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      })
+      .complete(() => {
+        const userKamaleon = new User({
+          name: 'Kamaleon',
+          email: 'nfe@kamaleon.com.br',
+          password: 'ow02-c99',
+        });
+
+        userKamaleon.save();
+      });
   },
 
   down: (queryInterface) => {
