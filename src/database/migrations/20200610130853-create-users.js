9d1cc06518a42879ca507ba9d690aaca1d5d50d8
Alex Aquino <alexqno@gmail.com>
2020-06-22 15:38:26 -0300

    Cria usuario kamaleon na migration

--- src/database/migrations/20200610130853-create-users.js
+++ src/database/migrations/20200610130853-create-users.js
@@ -1,46 +1,48 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface
-      .createTable('users', {
-        id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
+    const ret = queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Kamaleon',
+          email: 'nfe@kamaleon.com.br',
+          password: 'ow02-c99',
         },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password_hash: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        created_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
-        updated_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
-      })
-      .complete(() => {
-        queryInterface.sequelize.query(
-          queryInterface.QueryGenerator.bulkInsertQuery('users', [
-            {
-              name: 'Kamaleon',
-              email: 'nfe@kamaleon.com.br',
-              password: 'ow02-c99',
-            },
-          ])
-        );
-      });
+      ],
+      {}
+    );
+
+    return ret;
   },
 
   down: (queryInterface) => {
